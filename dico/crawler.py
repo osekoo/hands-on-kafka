a87d297ac70ca0ac83e94390bec59d5ca42c6862
kodjo <kodjo@accelextech.com>
2021-04-19 16:44:00 +0200

    Add Kafka cluster Add consumer code Add producer code

--- dico/crawler.py
+++ dico/crawler.py
@@ -17,22 +17,17 @@ class CrawlerFR(Crawler):
         page = requests.get(request_url)
         soup_handler = BeautifulSoup(page.content, 'html.parser')
         definition_elt = soup_handler.select_one('body > div.ws-c > main > section.def > div.b')
-        content = definition_elt.text
-        print(f'content: {content}')
-        content = re.sub(r'(\n\s*)+\n+', '\n\n', content)
-        print(f'content: {content}')
-        return content if definition_elt else None
+        return re.sub(r'(\n\s*)+\n+', '\n\n', definition_elt.text) if definition_elt else None
 
 
 class CrawlerEN(Crawler):
 
     def __init__(self):
-        self.base_url = 'https://dictionnaire.lerobert.com/definition/{word}'
+        self.base_url = 'https://www.dictionary.com/browse/{word}'
 
     def get_definition(self, word: str):
         request_url = self.base_url.replace('{word}', word)
         page = requests.get(request_url)
         soup_handler = BeautifulSoup(page.content, 'html.parser')
-        definition_elt = soup_handler.select_one('body > div.ws-c > main > section.def > div.b')
-
-        return definition_elt.text if definition_elt else None
+        definition_elt = soup_handler.select_one('#base-pw > main > section > section > div:nth-child(2) > section.css-pnw38j.e1hk9ate4 > div')
+        return re.sub(r'(\n\s*)+\n+', '\n\n', definition_elt.text) if definition_elt else None
